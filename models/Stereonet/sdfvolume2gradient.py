3535900e870862f14fb2d2b7b5e28993cd7c58ae
Liu Zihua <lukeliu2020@126.com>
2023-05-19 23:27:55 +0900

    add

---
+++ models/Stereonet/sdfvolume2gradient.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+def SDF2Graident(sdf_volume, K, ):
+    pass
+
+
+
+if __name__=="__main__":
+    sample = torch.rand(2,24,100,100)
+    SDF2Graident(sample)
+    pass

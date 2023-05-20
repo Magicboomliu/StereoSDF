6d1d69a7195e1a8e47dbb5402e363f7b9c45ac9c
Beniko95J <beniko_j@hotmail.com>
2023-05-20 12:50:19 +0900

    implement sdf2gradient

--- models/Stereonet/sdfvolume2gradient.py
+++ models/Stereonet/sdfvolume2gradient.py
@@ -4,9 +4,37 @@ import torch.nn.functional as F
 
 
 
-def SDF2Graident(sdf_volume, K, ):
-    pass
+def SDF2Graident(sdf_volume, K, hypo_depths):
+    '''
+    SDF_volume: (B, D, H, W)
+    K: (3, 3)
+    hypo_depths: (D, )
 
+    SDF_volume为1/8分辨率下
+    注意K也要被scale到1/8分辨率
+    '''
+    batch_num, depth_num, height, width = sdf_volume.shape
+    # i, j = torch.meshgrid(torch.linspace(0, width-1, width), torch.linspace(0, height-1, height), indexing='xy')
+    # coords_2d = torch.stack([i, j, torch.ones_like(i)], dim=0)  # (3, H, W)
+    # inv_K = torch.linalg.inv(K)
+    # coords_3d = torch.einsum('ij,jhw->ihw', inv_K, coords_2d)  # (3, H, W)
+    
+    # hypo_depths = hypo_depths[None, :, None, None]  # (1, D, 1, 1)
+    # coords_3d = coords_3d[:, None, ...] * hypo_depths  # (3, D, H, W)
+
+    z_diffs = torch.abs(hypo_depths[:-1] - hypo_depths[1:])  # (D-1, )
+    hypo_xs = 1 / K[0] * hypo_depths  # (D, )
+    hypo_ys = 1 / K[1] * hypo_depths  # (D, )
+
+    grad_x = torch.abs(sdf_volume[:, :, :, :-1] - sdf_volume[:, :, :, 1:]) / hypo_xs[None, :, None, None]  # (B, D, H, W-1)
+    grad_y = torch.abs(sdf_volume[:, :, :-1, :] - sdf_volume[:, :, 1:, :]) / hypo_ys[None, :, None, None]  # (B, D, H-1, W)
+    grad_z = torch.abs(sdf_volume[:, :-1, :, :] - sdf_volume[:, 1:, :, :]) / z_diffs[None, :, None, None]  # (B, D-1, H, W)
+
+    sdf_gradient = torch.stack([grad_x[:, depth_num-1, height-1, width-1],
+                                grad_y[:, depth_num-1, height-1, width-1],
+                                grad_z[:, depth_num-1, height-1, width-1]], dim=1)  # (B, 3, D-1, H-1, W-1)
+
+    return sdf_gradient
 
 
 if __name__=="__main__":

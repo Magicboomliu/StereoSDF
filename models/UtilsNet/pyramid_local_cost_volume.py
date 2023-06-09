b787e5b98b54fc194e7c03c15235a2008d23724c
Liu Zihua <lukeliu2020@126.com>
2023-06-09 17:20:20 +0900

    update local volume

--- models/UtilsNet/pyramid_local_cost_volume.py
+++ models/UtilsNet/pyramid_local_cost_volume.py
@@ -8,7 +8,6 @@ def ste_ceil(x):
 def ste_floor(x):
     return torch.floor(x) - x.detach() +x
 
-
 def normalize_coords(grid):
     """Normalize coordinates of image scale to [-1, 1]
     Args:
@@ -48,6 +47,7 @@ def build_local_cost_volume_based_on_feature(left_feat,right_feat,cur_disp,searc
     
     W_C = left_feat.shape[-1]
     W_D = cur_disp.shape[-1]
+    batch = left_feat.shape[0]
 
     scale = W_C//W_D
     if scale!=1:
@@ -64,32 +64,86 @@ def build_local_cost_volume_based_on_feature(left_feat,right_feat,cur_disp,searc
     sampling_candiate_intervals = addition_summation * sample_intervals
     
     sampling_candidates =lower_bound + sampling_candiate_intervals
-
-    # possible disparities: should bigger than zero and smaller than w
+    sampling_candidates = torch.clamp(sampling_candidates,min=0,max=W_C-1)
+    
+    # possible disparities: should bigger than zero and smaller than w.
     sample_candidate_ceil = ste_ceil(sampling_candidates)
     sample_candidate_floor = ste_floor(sampling_candidates)
+
+    # valid masks.
+    valid_ceil_candidates = (sample_candidate_ceil>=0)*(sample_candidate_ceil<=W_C-1)
+    valid_floor_candidates = (sample_candidate_floor>=0)*(sample_candidate_floor<=W_C-1)
+    
 
     sample_candidate_ceil = torch.clamp(sample_candidate_ceil,min=0,max=W_C-1)
     sample_candidate_floor = torch.clamp(sample_candidate_floor,min=0,max=W_C-1)
 
-
+    padding_model = 'border'
     
-    sample_candidates = sample_candidate_ceil.shape[1]
-    print(sample_candidates)
+    sample_candidates_num = sample_candidate_ceil.shape[1]
 
-
-    # grid = meshgrid(left_feat)  # [B, 2, H, W] in image scale
-    # # Note that -disp here
-    # offset = torch.cat((-disp, torch.zeros_like(disp)), dim=1)  # [B, 2, H, W]
-    # sample_grid = grid + offset
-    # sample_grid = normalize_coords(sample_grid)  # [B, H, W, 2] in [-1, 1]
-    # warped_feature = F.grid_sample(right_feat, sample_grid, mode='bilinear', padding_mode='border')
-
-
-    print(sample_candidate_ceil.shape)
-    print(sample_candidate_floor.shape)
-
-
+    # ceil local cost volume
+    ceil_local_volume = torch.zeros_like(sample_candidate_ceil)
+    # sample the left correlation of ceil
+    for i in range(sample_candidates_num):
+        grid = meshgrid(left_feat)
+        cur_disp = sample_candidate_ceil[:,i:i+1,:,:]
+        
+        offset = torch.cat((-cur_disp,torch.zeros_like(cur_disp)),dim=1) # [B,2,H,W]
+        
+        sample_grid  = grid + offset
+        
+        sample_grid = normalize_coords(sample_grid) # [B, H, W, 2] in [-1, 1]
+        # make sure sample from the right feature.
+        warped_feat = F.grid_sample(right_feat, sample_grid, mode='bilinear', padding_mode=padding_model)
+        
+        mask = torch.ones_like(right_feat)
+        valid_mask = F.grid_sample(mask,sample_grid,mode='bilinear',padding_mode=padding_model)
+        valid_mask[valid_mask<0.9999] =0
+        valid_mask[valid_mask>0] = 1
+        warped_feat = warped_feat
+        
+        correlation_value = (left_feat * warped_feat * valid_mask).mean(dim=1)
+        correlation_value = correlation_value.unsqueeze(1)
+        ceil_local_volume[:,i:i+1,:,:] = correlation_value
+    
+    ceil_local_volume = ceil_local_volume * valid_ceil_candidates
+    
+    
+    # floor local cost volume
+    floor_local_volume = torch.zeros_like(sample_candidate_floor)
+    # sample the left correlation of the follor
+    for i in range(sample_candidates_num):
+        grid = meshgrid(left_feat)
+        cur_disp = sample_candidate_floor[:,i:i+1,:,:]
+        offset = torch.cat((-cur_disp,torch.zeros_like(cur_disp)),dim=1) # [B,2,H,W]
+        
+        sample_grid  = grid + offset
+        sample_grid = normalize_coords(sample_grid) # [B, H, W, 2] in [-1, 1]
+        # make sure sample from the right feature.
+        warped_feat = F.grid_sample(right_feat, sample_grid, mode='bilinear', padding_mode=padding_model)
+        
+        mask = torch.ones_like(right_feat)
+        valid_mask = F.grid_sample(mask,sample_grid,mode='bilinear',padding_mode=padding_model)
+        valid_mask[valid_mask<0.9999] =0
+        valid_mask[valid_mask>0] = 1
+        warped_feat = warped_feat
+        
+        correlation_value = (left_feat * warped_feat * valid_mask).mean(dim=1)
+        correlation_value = correlation_value.unsqueeze(1)
+        floor_local_volume[:,i:i+1,:,:] = correlation_value
+    
+    floor_local_volume = floor_local_volume * valid_floor_candidates
+    
+    # final local cost volume
+    floor_rate =(sample_candidate_ceil- sampling_candidates)
+    ceil_rate = 1.0 - floor_rate
+    final_volume = ceil_local_volume*ceil_rate+ floor_local_volume*floor_rate
+    
+    return final_volume,sampling_candidates
+    
+    
+    
 
 
 def build_local_cost_volume_fixed(cost_volume,cur_disp,searching_radius,sample_nums):
@@ -123,6 +177,9 @@ def build_local_cost_volume_fixed(cost_volume,cur_disp,searching_radius,sample_n
     final_volume = ceil_volume*ceil_rate+ floor_volume*floor_rate
     
     return final_volume
+
+
+
 
 
 class PyrmaidCostVolume(nn.Module):
@@ -160,12 +217,62 @@ class PyrmaidCostVolume(nn.Module):
     
         return out
 
-
+import sys
+sys.path.append("../..")
+from utils.file_io import read_disp,read_img
+import matplotlib.pyplot as plt
 if __name__=="__main__":
-    feat1 = torch.randn(1,64,100,100)
-    feat2 = torch.randn(1,64,100,100)
+
+    feat1 = torch.relu(torch.randn(2,64,100,100))
+    feat2 = torch.relu(torch.randn(2,64,100,100))
     raduius = 3
-    cur_disparity = torch.abs(torch.randn(1,1,100,100)*10-6)
+    cur_disparity = torch.abs(torch.randn(2,1,100,100)*10-6)
     cur_disparity = torch.clamp(cur_disparity,min=0,max=99)
 
-    build_local_cost_volume_based_on_feature(feat1,feat2,cur_disp=cur_disparity,searching_radius=3)
+    final_volume,sampling_candidates = build_local_cost_volume_based_on_feature(feat1,feat2,cur_disp=cur_disparity,searching_radius=3)
+    
+    
+    # disp_path = "/home/zliu/SceneFlow_SAMPLE/disp.pfm"
+    # left_img_path = "/home/zliu/SceneFlow_SAMPLE/left.png"
+    # right_img_path = "/home/zliu/SceneFlow_SAMPLE/right.png"
+    
+    
+    # left_data = read_img(left_img_path)
+    # right_data = read_img(right_img_path)
+    # disp_data = read_disp(disp_path)
+    
+    
+    # left_data_tensor = torch.from_numpy(left_data).permute(2,0,1).unsqueeze(0)
+    # right_data_tensor = torch.from_numpy(right_data).permute(2,0,1).unsqueeze(0)
+    # disp_data_tensor = torch.from_numpy(disp_data).unsqueeze(0).unsqueeze(0)
+
+
+    
+    # padding_mode ='border'
+    # grid = meshgrid(left_data_tensor)
+    # offset = torch.cat((-disp_data_tensor, torch.zeros_like(disp_data_tensor)), dim=1)  # [B, 2, H, W]
+    # sample_grid = grid + offset
+    # sample_grid = normalize_coords(sample_grid)  # [B, H, W, 2] in [-1, 1]
+    # warped_img = F.grid_sample(right_data_tensor, sample_grid, mode='bilinear', padding_mode=padding_mode)
+
+
+    # mask = torch.ones_like(right_data_tensor)
+    # valid_mask = F.grid_sample(mask, sample_grid, mode='bilinear', padding_mode='zeros')
+    # valid_mask[valid_mask < 0.9999] = 0
+    # valid_mask[valid_mask > 0] = 1
+    
+    # warped_img = warped_img * valid_mask
+        
+    # error = torch.sum(torch.abs(warped_img-left_data_tensor),dim=1,keepdim=True)
+    
+    
+    # plt.subplot(3,1,1)
+    # plt.axis('off')
+    # plt.imshow(left_data/255)
+    # plt.subplot(3,1,2)
+    # plt.imshow(right_data/255)
+    # plt.axis('off')
+    # plt.subplot(3,1,3)
+    # plt.axis('off')
+    # plt.imshow(error.squeeze(0).squeeze(0).cpu().numpy(),cmap='gray')
+    # plt.show()
